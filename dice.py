851becdc79cd68711c4b5f51989e01310c9121e5
Andy Zhang <andyzhang19980520@gmail.com>
2014-04-17 19:28:56 -0400

    ask the number of sides each time

--- dice.py
+++ dice.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 import library
 import random
-side = library.int_input("How many sides do you want? >")
 time = library.int_input("How many times do you want to roll >")
-for x in range(1,time):
+for x in range(0,time):
+    side = library.int_input("How many sides do you want? >")
     num = random.randint(1,side)
     print num
