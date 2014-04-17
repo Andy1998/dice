294657622aa24ad98232021fd90bb106a2d8c35b
Andy Zhang <andyzhang19980520@gmail.com>
2014-04-17 19:49:08 -0400

    the emergency

--- dice.py
+++ dice.py
@@ -2,6 +2,8 @@
 import library
 import random
 side = library.int_input("How many sides do you want? >")
+while side < 2:
+    side = library.int_input("The dice should have at least one side. Try again >")
 time = library.int_input("How many times do you want to roll >")
 for x in range(1,time):
     num = random.randint(1,side)
